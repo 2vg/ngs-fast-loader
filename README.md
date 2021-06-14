b5cc119ad078360c1a5b0c766b890f0a0b708cad
uru <mail@nyaa.gg>
2021-06-14 11:48:27 +0900

    feat: 1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+ngs-fast-loader
+===
+
+NGSのゲームスタート前のクソ長いファイルチェックをスキップし、ゲームへ素早くログインするためのツールです。</br>
+具体的にはユーザーの`Documents\SEGA\PHANTASYSTARONLINE2`にある`_version.ver`というファイルを`version.ver` にコピーするだけですが、消えたり消えなかったりしてめんどくさいのでツールでディレクトリを監視させて消えたらコピーするだけ、という感じです。</br>
+どうやらアップデートのための追加ファイル等を逐一チェックしているようなので、これをずっと使い続けるのはよくないです。</br>
+再起動するだけで、すぐにゲーム戻るのにチェックいらんべ！とかいう時にお使いください。</br>
+後なんか問題起きても私は責任を負いかねます。自己責任で。</br>
+
+## Usage
+
+`ngs-fast-loader.exe`を先に起動した状態で、NGSのランチャーを起動してください。</br>
+うまく起動してたら黒い背景のよくわからんウィンドウが出てきて、そこに`pso2 fast loader started`みたいな文字が出てきます。</br>
+あとは`version.ver`が消えたら勝手にコピーしてくれます。</br>
+止めたかったら黒い背景のよくわからんウィンドウを閉じてください。</br>
+
+## Download
+
+こちらから: [Release]()
+
+## Build
+
+rust入れといてね
+
+```shell
+git clone https://github.com/2vg/ngs-fast-loader
+cd ngs-fast-loader
+cargo build --release
+```
---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use anyhow::*;
+use directories::UserDirs;
+use env_logger::Builder;
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use log::{error, info};
+use notify::{Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::{
+    ffi::OsStr,
+    fs,
+    path::{Path, PathBuf},
+};
+
+fn get_verion_file_path(only_directory: bool, with_under_score: bool) -> Result<PathBuf> {
+    if let Some(user_dirs) = UserDirs::new() {
+        if let Some(document_dir) = user_dirs.document_dir() {
+            if only_directory {
+                return Ok(document_dir.join(r"SEGA\PHANTASYSTARONLINE2"));
+            }
+
+            if with_under_score {
+                return Ok(document_dir.join(r"SEGA\PHANTASYSTARONLINE2\_version.ver"));
+            } else {
+                return Ok(document_dir.join(r"SEGA\PHANTASYSTARONLINE2\version.ver"));
+            }
+        } else {
+            bail!("Not found User Documents directory.");
+        }
+    } else {
+        bail!("Not found User directory.");
+    }
+}
+
+fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+
+    let watcher = RecommendedWatcher::new(move |res| {
+        futures::executor::block_on(async {
+            tx.send(res).await.unwrap();
+        })
+    })?;
+
+    Ok((watcher, rx))
+}
+
+async fn async_watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (mut watcher, mut rx) = async_watcher()?;
+
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => match event.kind {
+                EventKind::Remove(_) => {
+                    if &event.paths.len() != &0 {
+                        let removed_path = &event.paths[0];
+
+                        if removed_path.file_name().unwrap_or(OsStr::new("foo")) != "version.ver" {
+                            continue;
+                        }
+
+                        if let Ok(under_score_version_file) = get_verion_file_path(false, true) {
+                            if !under_score_version_file.exists() {
+                                error!("Not found _version.ver file");
+                            }
+
+                            if let Ok(_) = fs::copy(&under_score_version_file, &removed_path) {
+                                info!("Success to copy _version.ver to version.ver");
+                            }
+                        };
+                    }
+                }
+                _ => {}
+            },
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let mut builder = Builder::new();
+    builder.filter(None, log::LevelFilter::Trace);
+    builder.init();
+
+    info!("pso2 fast loader started");
+
+    let path = get_verion_file_path(true, false)?;
+
+    futures::executor::block_on(async {
+        if let Err(e) = async_watch(path).await {
+            println!("error: {:?}", e)
+        }
+    });
+
+    Ok(())
+}
